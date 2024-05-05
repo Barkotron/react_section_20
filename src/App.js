5365e0d106f65c5170f9fe4b2a9c67f9fdce1abc
Jackson Barker <jcrb45@hotmail.com>
2024-05-05 10:07:05 -0500

    added conection to firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
-import uiSlice from './store/ui-slice';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import uiSlice from "./store/ui-slice";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  useEffect(() => {
+    fetch(
+      "https://react-redux-cart-79656-default-rtdb.firebaseio.com/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    ); //.json is firebase specific
+  }, [cart]);
 
   return (
     <Layout>
-      { showCart && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
